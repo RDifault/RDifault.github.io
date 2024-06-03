da156d38fce45c567ca6e10fce76565150e7c0c5
Ramadhian Eka Putra <95353094+RDifault@users.noreply.github.com>
2024-06-03 15:23:44 +0700

    Create data.js

---
+++ scripts/data.js
@@ -0,0 +1,607 @@
+const projects = {
+  proj: [
+    {
+      id: 1,
+      title: "Machine/Deep Learning",
+      desc: "I have done many projects involving machine learning and deep learning. I have learned many concepts like SVM, RF, to neural network concepts and its models like RNN, CNN, etc.",
+      img: "assets/ml0-1.png",
+      detail: [
+        {
+          tools: [
+            {
+              id: 1,
+              image: "assets/py.png",
+              bullet: "Python",
+            },
+            {
+              id: 2,
+              image: "assets/Tensorflow.png",
+              bullet: "Tensorflow",
+            },
+            {
+              id: 3,
+              image: "assets/matplotlib.png",
+              bullet: "MatPlotlib",
+            },
+            {
+              id: 4,
+              image: "assets/scipy.png",
+              bullet: "Scipy",
+            },
+            {
+              id: 5,
+              image: "assets/keras.png",
+              bullet: "Keras",
+            },
+            {
+              id: 6,
+              image: "assets/WFDB.png",
+              bullet: "WFDB",
+            },
+          ],
+          concepts: [
+            {
+              id: 1,
+              con: "SVM",
+            },
+            {
+              id: 2,
+              con: "RF",
+            },
+            {
+              id: 3,
+              con: "CNN",
+            },
+            {
+              id: 4,
+              con: "GRU",
+            },
+            {
+              id: 5,
+              con: "LSTM",
+            },
+            {
+              id: 6,
+              con: "Autoencoder",
+            },
+          ],
+          aboutProject1:
+            "I have done many projects involving machine learning and deep learning. Started with machine learning, i have learnt many machine learning concepts like Support Vector Machine and Random Forest for binary and multi-class classification. I have also learned Principal Component Analysis for dimensional reduction.",
+          aboutProject2:
+            "Moving on to deep learning, i have learned neural network concepts and its models like Recurrent Neural Network, Convolutional Neural Network, Autoencoder, and many more. I have wrote a research paper in this field about sleep apnea classification using ECG signal using the combination of autoencoder and CNN+GRU, currently under review by a journal.",
+          aboutProject3:
+            "Check out my machine learning and deep learning projects through my Github",
+          githubLink:
+            "https://github.com/RDifault/Machine-and-Deep-Learning-Projects",
+          projectGallery: [
+            {
+              id: 1,
+              text: "Parkinson Detection using ANN",
+              image: "assets/ml_01.png",
+            },
+            {
+              id: 2,
+              text: "Fuzzy C-Means Clustering",
+              image: "assets/ml_02.png",
+            },
+            {
+              id: 3,
+              text: "Linear Regression to Predict Fuel Consumption",
+              image: "assets/ml_03.png",
+            },
+            {
+              id: 4,
+              text: "Regression using RBF Neural Network",
+              image: "assets/ml_04.png",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 2,
+      title: "Computer Vision",
+      desc: "I have learned a lot about computer vision in the university and have made several projects in it. I mainly uses OpenCV2 to do computer vision projects",
+      img: "assets/cv_1.png",
+      detail: [
+        {
+          tools: [
+            {
+              id: 1,
+              image: "assets/py.png",
+              bullet: "Python",
+            },
+            {
+              id: 2,
+              image: "assets/seaborn.png",
+              bullet: "Seaborn",
+            },
+            {
+              id: 3,
+              image: "assets/matplotlib.png",
+              bullet: "MatPlotlib",
+            },
+            {
+              id: 4,
+              image: "assets/opencv.png",
+              bullet: "OpenCV",
+            },
+            {
+              id: 5,
+              image: "assets/scipy.png",
+              bullet: "Scipy",
+            },
+            {
+              id: 6,
+              image: "assets/pandas.png",
+              bullet: "Pandas",
+            },
+          ],
+          concepts: [
+            {
+              id: 1,
+              con: "SIFT",
+            },
+            {
+              id: 2,
+              con: "FLANN",
+            },
+            {
+              id: 3,
+              con: "HOG",
+            },
+            {
+              id: 4,
+              con: "SVM",
+            },
+            {
+              id: 5,
+              con: "Canny Edge",
+            },
+            {
+              id: 6,
+              con: "BFMatcher",
+            },
+          ],
+          aboutProject1:
+            "I have learned a lot about computer vision in the university and have made several projects in it. I mainly uses OpenCV2 to do computer vision projects",
+          aboutProject2:
+            "In this subject, i have understood the fundamentals of computer vision and implement it into my projects. My projects involves the use of SVM, PCA, and Histogram of Oriented Gradients (HoG) to do face recognition, image matching using SIFT, Canny Edge Detection, and many more",
+          aboutProject3:
+            "The complete collection of my projects can be seen through my Github",
+          githubLink: "https://github.com/RDifault/Computer-Vision",
+          projectGallery: [
+            {
+              id: 1,
+              text: "Harris Corner, FLANN, and BFMatcher",
+              image: "assets/cv_1.png",
+            },
+            {
+              id: 2,
+              text: "Face Recognition using PCA and SVM",
+              image: "assets/cv_2.png",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 3,
+      title: "Web Development",
+      desc: "I'm also quiet proficient with web development. Some of the demonstration of my web development skill is in this portfolio website, which i made purely using HTML, CSS, and Javascript without any framework",
+      img: "assets/wd-1.png",
+      detail: [
+        {
+          tools: [
+            {
+              id: 1,
+              image: "assets/html.png",
+              bullet: "HTML",
+            },
+            {
+              id: 2,
+              image: "assets/css.png",
+              bullet: "CSS",
+            },
+            {
+              id: 3,
+              image: "assets/Javascript.png",
+              bullet: "Javascript",
+            },
+            {
+              id: 4,
+              image: "assets/vue.png",
+              bullet: "Vue",
+            },
+            {
+              id: 5,
+              image: "assets/bootstrap.png",
+              bullet: "Bootstrap",
+            },
+            {
+              id: 6,
+              image: "assets/yarn.png",
+              bullet: "Yarn",
+            },
+          ],
+          concepts: [
+            {
+              id: 1,
+              con: "Frontend",
+            },
+            {
+              id: 2,
+              con: "UI/UX Design",
+            },
+            {
+              id: 3,
+              con: "Backend",
+            },
+            {
+              id: 4,
+              con: "DOM",
+            },
+            {
+              id: 5,
+              con: "JSON",
+            },
+            {
+              id: 6,
+              con: "Framework",
+            },
+          ],
+          aboutProject1:
+            "I'm also quiet proficient with web development since i've also made projects in it. Some of the demonstration of my web development skill is in this portfolio website, which i made purely using HTML, CSS, and Javascript without any framework",
+          aboutProject2:
+            "One of the web framework i have learned are Vue.js, which is a frontend framework that i used when i was doing my internship at Alfagift.",
+          aboutProject3:
+            "currently i have 4 projects in web development. 2 projects are available to public and made with my friend, while the other 2 are company's project. Check out the project through this github link",
+          githubLink: "https://github.com/Valenst/HCI-ABABA",
+          projectGallery: [
+            {
+              id: 1,
+              text: "ABABA Restaurant and Reservation",
+              image: "assets/wd_01.png",
+            },
+            {
+              id: 2,
+              text: "Alfagift Website",
+              image: "assets/wd_02.png",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 4,
+      title: "Laravel",
+      desc: " I have learned Laravel basics in the university and enhances my skill by making project. Currently i have made 1 Laravel project with my friend.",
+      img: "assets/laravel-thumbnail-2.jpg",
+      detail: [
+        {
+          tools: [
+            {
+              id: 1,
+              image: "assets/laravel.png",
+              bullet: "Laravel",
+            },
+            {
+              id: 2,
+              image: "assets/xampp.png",
+              bullet: "XAMPP",
+            },
+            {
+              id: 3,
+              image: "assets/html.png",
+              bullet: "HTML",
+            },
+            {
+              id: 4,
+              image: "assets/css.png",
+              bullet: "CSS",
+            },
+            {
+              id: 5,
+              image: "assets/npm.png",
+              bullet: "NPM",
+            },
+            {
+              id: 6,
+              image: "assets/php.png",
+              bullet: "PHP",
+            },
+          ],
+          concepts: [
+            {
+              id: 1,
+              con: "Server-Side",
+            },
+            {
+              id: 2,
+              con: "Backend",
+            },
+            {
+              id: 3,
+              con: "Routing",
+            },
+            {
+              id: 4,
+              con: "Database",
+            },
+            {
+              id: 5,
+              con: "PHP",
+            },
+            {
+              id: 6,
+              con: "Web-Framework",
+            },
+          ],
+          aboutProject1:
+            " I have learned Laravel basics in the university and enhances my skill by making project. Currently i have made 1 Laravel project with my friend.",
+          aboutProject2:
+            "I have learned design pattern concept of Model-View-Controller (MVC) using Laravel. I have also learned how to connect the web app to database through XAMPP. The frontend side of blade file is made using pure HTML and CSS",
+          aboutProject3: "You can check our project through this github link",
+          githubLink: "https://github.com/Valenst/HCI-ABABA",
+          projectGallery: [
+            {
+              id: 1,
+              text: "ABABA Restaurant and Reservation",
+              image: "assets/wd_01.png",
+            },
+            {
+              id: 2,
+              text: "Alfagift Website",
+              image: "assets/wd_02.png",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 5,
+      title: "UI/UX Design",
+      desc: " I'm also proficient in using Figma to produce UI and UX. Through my internship, i have done steps to develop an efficient UI.",
+      img: "assets/ui-1.png",
+      detail: [
+        {
+          tools: [
+            {
+              id: 1,
+              image: "assets/figma-2.png",
+              bullet: "Figma",
+            },
+            {
+              id: 2,
+              image: "assets/illustrator.png",
+              bullet: "Adobe Illustrator",
+            },
+            {
+              id: 3,
+              image: "assets/coreldraw.png",
+              bullet: "CorelDraw",
+            },
+          ],
+          concepts: [
+            {
+              id: 1,
+              con: "UI Design",
+            },
+            {
+              id: 2,
+              con: "UX Research",
+            },
+            {
+              id: 3,
+              con: "Usability Testing",
+            },
+            {
+              id: 4,
+              con: "Design System",
+            },
+            {
+              id: 5,
+              con: "Prototyping",
+            },
+            {
+              id: 6,
+              con: "Wireframing",
+            },
+          ],
+          aboutProject1:
+            "I'm also proficient in using Figma to produce UI and UX. Through my internship i have done steps to develop an efficient UI. Starting from user interview, making affinity diagram, making wireframe, and develop the final UI and its detailed prototype",
+          aboutProject2:
+            " I have done 1 big project in UI Design when i was in my internship in Alfagift. I'm involved in making the UI of refund system with the help of my mentor. I have also done a UI redesign when i was in bootcamp.",
+          aboutProject3: "You can check my project through this figma link",
+          figmaLink: "https://github.com/Valenst/HCI-ABABA",
+          projectGallery: [
+            {
+              id: 1,
+              text: "MySkill.id UI Redesign",
+              image: "assets/ui-1.png",
+            },
+            {
+              id: 2,
+              text: "Alfagift Mobile App",
+              image: "assets/alfagift.png",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 6,
+      title: "Spark Data Analysis",
+      desc: "I have also done many data analysis using Spark. The analysis is done using the combination of Python and SQL",
+      img: "assets/sda-thumb.png",
+      detail: [
+        {
+          tools: [
+            {
+              id: 1,
+              image: "assets/py.png",
+              bullet: "Python",
+            },
+            {
+              id: 2,
+              image: "assets/sql-2.png",
+              bullet: "SQL",
+            },
+            {
+              id: 3,
+              image: "assets/spark.png",
+              bullet: "Spark",
+            },
+            {
+              id: 4,
+              image: "assets/matplotlib.png",
+              bullet: "MatPlotLib",
+            },
+          ],
+          concepts: [
+            {
+              id: 1,
+              con: "EDA",
+            },
+            {
+              id: 2,
+              con: "Plotting",
+            },
+            {
+              id: 3,
+              con: "Machine Learning",
+            },
+            {
+              id: 4,
+              con: "SQL Query",
+            },
+            {
+              id: 5,
+              con: "Data Preprocessing",
+            },
+            {
+              id: 6,
+              con: "Data Transformation",
+            },
+          ],
+          aboutProject1:
+            "I have also done many data analysis using Spark. The analysis is done using the combination of Python and SQL. I have learned many concepts in data analysis like exploratory data analysis, preprocessing the data for machine learning, and classification using machine learning",
+          aboutProject2:
+            "Majorly i used PySpark and google Colab to do the data analysis. In that case, i have to be proficient in python and SQL",
+          aboutProject3: "My projects are available in Github",
+          githubLink: "https://github.com/RDifault/Spark-Data-Analysis",
+          projectGallery: [
+            {
+              id: 1,
+              text: "Crime Data Analysis",
+              image: "assets/sda-01.png",
+            },
+            {
+              id: 2,
+              text: "Ramen Restaurant Data Analysis",
+              image: "assets/sda-02.png",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 7,
+      title: "Data Warehouse",
+      desc: "I have made a project in my university transforming data from ERD to Business Intelligence",
+      img: "assets/etl-thumb.avif",
+      detail: [
+        {
+          tools: [
+            {
+              id: 1,
+              image: "assets/power-bi.png",
+              bullet: "Power BI",
+            },
+            {
+              id: 2,
+              image: "assets/ms-sql.png",
+              bullet: "MS SQL",
+            },
+            {
+              id: 3,
+              image: "assets/pentaho.png",
+              bullet: "Pentaho Spoon",
+            },
+          ],
+          concepts: [
+            {
+              id: 1,
+              con: "ERD",
+            },
+            {
+              id: 2,
+              con: "ETL",
+            },
+            {
+              id: 3,
+              con: "SQL Query",
+            },
+            {
+              id: 4,
+              con: "Business Intelligence",
+            },
+          ],
+          aboutProject1:
+            "I have made a project in my university transforming data from ERD to Business Intelligence. i got a project where i need to demonstrate the transformation process of raw data to readable business intelligence.",
+          aboutProject2:
+            " The process of transforming the data demonstrated using a Spotify demo ERD. Then, create database using SQL and add sample datas. Then, transform the data using Extract, Transform, Load process in Pentaho Spoon. Then, display the datas to BI to be analyzed",
+          aboutProject3: "You can check my project through this Github link",
+          githubLink: "https://github.com/RDifault/Spotify-Data-Warehouse",
+          projectGallery: [
+            {
+              id: 1,
+              text: "Spotify Data Warehouse",
+              image: "assets/etl-01.png",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+const skills = {
+  hardSkills: [
+    "SQL",
+    "Python",
+    "Laravel",
+    "Vue.js",
+    "PHP",
+    "Javascript",
+    "Github",
+    "MySQL",
+    "Bootstrap",
+    "Figma",
+    "CSS",
+    "HTML",
+    "Machine Learning",
+    "Computer Vision",
+    "Responsive Design",
+    "Signal Processing",
+    "Neural Network",
+    "Spark",
+    "Data Analysis",
+  ],
+  softSkills: [
+    "Diligent",
+    "Analysis",
+    "Collaborative",
+    "Creative",
+    "Communicative",
+    "Flexibility",
+    "Problem Solving",
+    "Decision-Making",
+    "Motivated",
+  ],
+};
+
+// export {projects};
+
+// const json = JSON.stringify(projects);
+// console.info(json);
